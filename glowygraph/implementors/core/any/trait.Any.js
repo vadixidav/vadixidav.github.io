339938b24edbe2d3f7d8b63b36032a88b49012db
Geordon Worley <vadixidav@gmail.com>
2016-07-23 12:26:02 -0400

    glowygraph: 0.6 release

--- glowygraph/implementors/core/any/trait.Any.js
+++ glowygraph/implementors/core/any/trait.Any.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["libloading"] = [];implementors["shared_library"] = [];implementors["wayland_sys"] = [];implementors["tempfile"] = [];implementors["wayland_kbd"] = [];implementors["glutin"] = [];implementors["glium"] = [];implementors["glutin"] = [];implementors["glium"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["libloading"] = [];implementors["shared_library"] = [];implementors["wayland_sys"] = [];implementors["tempfile"] = [];implementors["wayland_kbd"] = [];implementors["glutin"] = [];implementors["glium"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- glowygraph/implementors/std/panic/trait.RecoverSafe.js
+++ glowygraph/implementors/std/panic/trait.RecoverSafe.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libloading"] = [];implementors["shared_library"] = [];implementors["tempfile"] = [];implementors["glutin"] = [];implementors["glium"] = [];implementors["glutin"] = [];implementors["glium"] = [];
+implementors["libloading"] = [];implementors["shared_library"] = [];implementors["tempfile"] = [];implementors["glutin"] = [];implementors["glium"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
